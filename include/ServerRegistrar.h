ff5cd4273b6905e9bc7ecee340c1e847cbfe7a39
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-04-08 16:15:16 +0530

    removed everything

--- include/ServerRegistrar.h
+++
@@ -1,9 +0,0 @@
-#include <uv.h>
-class ServerRegistrar{
-  uv_loop_t* eventLoop;
-  uv_tcp_t server;
-  int listeningPort;
-  int maxConnections;
-  public:
-  ServerRegistrar(int port);
-};
--- include/Stream.h
+++
@@ -1,5 +0,0 @@
-class Stream{
-  public:
-    virtual int read() = 0;
-    virtual const int write(const char* buffer, int size) = 0;
-};
