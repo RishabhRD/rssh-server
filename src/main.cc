cb38e28f775dc816a87a2fa0611c4fedde10207e
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-04-15 14:09:51 +0530

    added main

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,21 @@
-#include <iostream>
 #include "ServerListener.h"
+#include <iostream>
 using namespace std;
-int main() {
-  try{
-  asio::io_context context;
-  auto listener = ServerListener::create(context, 8080);
-  listener->startListening();
-  context.run();
-  }catch(std::system_error& error){
-    cerr<<error.what()<<endl;
+int main(int argc, char **argv) {
+  try {
+    if(argc != 2){
+      cerr<<"Usage: rssh-server <port>"<<endl;
+      return 1;
+    }
+    std::string port = argv[1];
+    int portNum = std::stoi(port);
+    asio::io_context context;
+    auto listener = ServerListener::create(context, portNum);
+    listener->startListening();
+    context.run();
+  } catch (std::system_error &error) {
+    cerr << error.what() << endl;
+  } catch (std::exception &ex) {
+    cerr << ex.what() << endl;
   }
 }
